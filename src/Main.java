0bd5a93af08a8ca8fae5b93e21c5fa616db4a428
Semyon Mikhailov <semen7721@gmail.com>
2023-04-19 18:07:41 +0300

    Добавляет комментарии

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Main {
+    // Метод Монте-Карло для численного интегрирования
     public double monteCarloIntegration(Calculator calculator, double bottomEdge, double topEdge, int stepCount) {
-        double Ymin = Double.MAX_VALUE;
-        double Ymax = Double.MIN_VALUE;
+        double Ymin = Double.MAX_VALUE; // Минимальное значение y
+        double Ymax = Double.MIN_VALUE; // Максимальное значение y
+        // Поиск минимального и максимального значения y на заданном интервале
         for (double i = bottomEdge; i <= topEdge; i++){
             double y = calculator.func(i);
             Ymin = Math.min(Ymin, y);
             Ymax = Math.max(Ymax, y);
         }
-        int Nbottom = 0;
+        int Nbottom = 0; // Количество точек под кривой
+        // Генерация случайных точек и подсчет количества точек под кривой
         for (int i = 0; i < stepCount; i++){
             double x = bottomEdge + Math.random() * (topEdge - bottomEdge);
             double y = Ymin + Math.random() * (Ymax - Ymin);
@@ -18,6 +21,7 @@ public class Main {
                 Nbottom++;
             }
         }
+        // Оценка площади под кривой функции
         return (topEdge - bottomEdge) * (Ymax - Ymin) * (double) Nbottom / (double) stepCount;
     }
 
@@ -25,9 +29,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
         Main integral = new Main();
-        double bottomEdge = scanner.nextDouble();
-        double topEdge = scanner.nextDouble();
-        int stepCount = scanner.nextInt();
-        System.out.println(integral.monteCarloIntegration(calculator, bottomEdge, topEdge, stepCount));
+        // Ввод пользовательских данных
+        System.out.print("Введите нижнюю границу интегрирования: ");
+        double bottomEdge = scanner.nextDouble(); // Нижний предел
+        System.out.print("Введите верхнюю границу интегрирования: ");
+        double topEdge = scanner.nextDouble(); // Верхний предел
+        System.out.print("Введите количество шагов: ");
+        int stepCount = scanner.nextInt(); // Количество шагов
+        // Вызов метода для оценки площади под кривой функции и вывод результата на экран
+        System.out.println("Результат интегрирования: " + integral.monteCarloIntegration(calculator, bottomEdge, topEdge, stepCount));
     }
 }
